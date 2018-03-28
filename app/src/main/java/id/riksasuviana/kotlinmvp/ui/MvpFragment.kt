de5e105c21ce2a071567eb9be76692548c74cd2b
Riksa Suviana <riksa_leone@yahoo.co.id>
2018-03-28 23:05:59 +0700

    fix fragment lifecycle bug

--- app/src/main/java/id/riksasuviana/kotlinmvp/ui/MvpFragment.kt
+++ app/src/main/java/id/riksasuviana/kotlinmvp/ui/MvpFragment.kt
@@ -1,24 +1,31 @@
 package id.riksasuviana.kotlinmvp.ui
 
+import android.app.ProgressDialog
 import android.os.Bundle
 import android.support.annotation.LayoutRes
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import id.riksasuviana.kotlinmvp.base.BasePresenter
 
 /**
  * Created by riksa on 28/01/2018.
  */
-abstract class MvpFragment<P : BasePresenter<Any>> : Fragment() {
+abstract class MvpFragment<P : BasePresenter<*>> : Fragment() {
     protected lateinit var mvpPresenter: P
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val rootView = inflater.inflate(getFragmentLayout(), container, false)
+        return inflater.inflate(getFragmentLayout(), container, false)
+    }
+
+    override fun onStart() {
+        super.onStart()
+
         mvpPresenter = createPresenter()
+
         doCreateFragment()
-        return rootView
     }
 
     abstract fun doCreateFragment()
@@ -28,4 +35,22 @@ abstract class MvpFragment<P : BasePresenter<Any>> : Fragment() {
     fun getFragmentLayout(): Int
 
     protected abstract fun createPresenter(): P
+
+    var mProgressDialog: ProgressDialog? = null
+
+    open fun showLoading(){
+        mProgressDialog = ProgressDialog(context)
+        mProgressDialog?.setCancelable(false)
+        mProgressDialog?.show()
+    }
+
+    open fun hideLoading(){
+        if(mProgressDialog != null){
+            mProgressDialog?.dismiss()
+        }
+    }
+
+    fun toast(message:String?, duration:Int){
+        Toast.makeText(context, message, duration).show()
+    }
 }
